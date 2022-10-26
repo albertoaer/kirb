d7d6ebb41f3266a81f2afb00352d3a6098183056
Alberto Elorza <albertoelorza2002@gmail.com>
2022-10-26 21:36:59 +0200

    http request parser

---
+++ lib/http/http_request.rb
@@ -0,0 +1,29 @@
+class HttpParseError < StandardError
+  def initialize msg
+    super msg
+  end
+end
+
+class HttpRequest
+  def initialize(verb, route, version, headers, body)
+    @verb = verb
+    @route = route
+    @version = version
+    @headers = headers
+    @body
+  end
+
+  def self.parse(raw)
+    bodysplit = raw.index("\r\n\r\n")
+    headerraw = raw[0..bodysplit-1]
+    bodyraw = raw[bodysplit+4..-1]
+
+    lines = headerraw.split("\r\n")
+    raise HttpParseError.new 'No headers' if lines.empty?
+    verb, route, version = lines.shift.match(/(\w+)\s+(.*?)\s+(.*)/).captures
+    raise HttpParseError.new 'Invalid http request format' unless verb and route and version
+    headers = lines.map { |ln| ln.match(/^([\w-]+): (.*)$/).captures }
+    raise HttpParseError.new 'Invalid http headers format' unless headers.all?
+    HttpRequest.new verb, route, version, headers, bodyraw
+  end
+end
