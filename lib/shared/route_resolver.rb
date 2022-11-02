b415a3ce9dfee25bbeb0fd52c269f183a47fcca8
Alberto Elorza <albertoelorza2002@gmail.com>
2022-11-02 19:05:00 +0100

    improve route resolver

--- lib/shared/route_resolver.rb
+++ lib/shared/route_resolver.rb
@@ -4,13 +4,23 @@ module Kirb
   class InvalidRoute < StandardError
   end
 
+  module RouteValidator
+    @@route_word = '[^?\/#:]+'
+    #TODO: Include query and fragment in the validation
+    @@rotue_regex = /^\/(#{@@route_word}(\/#{@@route_word})*)?$/
+    
+    def self.validate route
+      @@rotue_regex.match? route
+    end
+  end
+
   class RouteResolver
     # any valid character in route part
     @@route_char = '[^?\/#:]'
     # any valid method name
     @@method_match = (METHODS + [":#{@@route_char}*"]).join '|'
     # validation of route
-    @@route_regex = /^((#{@@method_match})#)?(\/((:#{@@route_char}*)|#{@@route_char}+))+\/?$/
+    @@route_regex = /^((#{@@method_match})#)?\/(((:#{@@route_char}*)|#{@@route_char}+)(\/((:#{@@route_char}*)|#{@@route_char}+))*\/?)?\/?$/
 
     attr_reader :matcher
 
@@ -19,8 +29,8 @@ module Kirb
       regex_route = route.gsub(/:([#{@@route_char}])*/) { |s|
         s.length == 1 ? "#{@@route_char}+" : "(?<#{s[1..-1]}>#{@@route_char}+)"
       }
-      regex_route = regex_route[0..-2] + '(?<*remain*>(\/.*)*)?' if regex_route[-1] == '/'
-      unless /^(#{@@method_match})#/
+      regex_route = regex_route[0..-3] + '(?<*remain*>(\/.*)+)' if regex_route[-2..] == '//'
+      unless regex_route =~ /^(#{@@method_match})#/
         regex_route = "(#{@@route_char}+#)?" + regex_route
       end
       @matcher = /^#{regex_route}$/
--- spec/shared/route_resolver_spec.rb
+++ spec/shared/route_resolver_spec.rb
@@ -1,16 +1,35 @@
 require_relative '../../lib/shared/route_resolver'
 
+describe Kirb::RouteValidator do
+  it 'wrong route' do
+    expect(Kirb::RouteValidator.validate '').to eq(false)
+    expect(Kirb::RouteValidator.validate '//').to eq(false)
+    expect(Kirb::RouteValidator.validate '/?/').to eq(false)
+    expect(Kirb::RouteValidator.validate '/#').to eq(false)
+  end
+
+  it 'right route' do
+    expect(Kirb::RouteValidator.validate '/').to eq(true)
+    expect(Kirb::RouteValidator.validate '/a').to eq(true)
+    expect(Kirb::RouteValidator.validate '/aa/b').to eq(true)
+    expect(Kirb::RouteValidator.validate '/aa/b/e').to eq(true)
+  end
+end
+
 describe Kirb::RouteResolver do
   it 'wrong route' do
-    expect { Kirb::RouteResolver.new '/' }.to raise_error(Kirb::InvalidRoute)
-    expect { Kirb::RouteResolver.new '//' }.to raise_error(Kirb::InvalidRoute)
+    expect { Kirb::RouteResolver.new '///' }.to raise_error(Kirb::InvalidRoute)
     expect { Kirb::RouteResolver.new '#/' }.to raise_error(Kirb::InvalidRoute)
     expect { Kirb::RouteResolver.new '#/route' }.to raise_error(Kirb::InvalidRoute)
     expect { Kirb::RouteResolver.new 'GET:#/route' }.to raise_error(Kirb::InvalidRoute)
     expect { Kirb::RouteResolver.new '/GET#/route' }.to raise_error(Kirb::InvalidRoute)
+    expect { Kirb::RouteResolver.new '/route///' }.to raise_error(Kirb::InvalidRoute)
+    expect { Kirb::RouteResolver.new 'GET#SET#/route/' }.to raise_error(Kirb::InvalidRoute)
   end
 
   it 'right route' do
+    Kirb::RouteResolver.new '//'
+    Kirb::RouteResolver.new '/'
     Kirb::RouteResolver.new '/example'
     Kirb::RouteResolver.new '/example/other'
     Kirb::RouteResolver.new '/example/other/'
@@ -21,15 +40,17 @@ describe Kirb::RouteResolver do
     Kirb::RouteResolver.new 'GET#/:example/:hey'
     Kirb::RouteResolver.new ':method#/:example/:hey'
     Kirb::RouteResolver.new ':GET#/route'
+    Kirb::RouteResolver.new ':GET#/route//'
   end
 
   it 'create matcher' do
     match = -> (r) { Kirb::RouteResolver.new(r).matcher.to_s }
     expect(match.('/a/b/c')).to eq(/^([^?\/#:]+#)?\/a\/b\/c$/.to_s)
-    expect(match.('/a/b/c/')).to eq(/^([^?\/#:]+#)?\/a\/b\/c(?<*remain*>(\/.*)*)?$/.to_s)
-    expect(match.('/a/:b/c/')).to eq(/^([^?\/#:]+#)?\/a\/(?<b>[^?\/#:]+)\/c(?<*remain*>(\/.*)*)?$/.to_s)
-    expect(match.('/:/:b/c/')).to eq(/^([^?\/#:]+#)?\/[^?\/#:]+\/(?<b>[^?\/#:]+)\/c(?<*remain*>(\/.*)*)?$/.to_s)
+    expect(match.('/a/b/c/')).to eq(/^([^?\/#:]+#)?\/a\/b\/c\/$/.to_s)
+    expect(match.('/a/:b/c/')).to eq(/^([^?\/#:]+#)?\/a\/(?<b>[^?\/#:]+)\/c\/$/.to_s)
+    expect(match.('/:/:b/c/')).to eq(/^([^?\/#:]+#)?\/[^?\/#:]+\/(?<b>[^?\/#:]+)\/c\/$/.to_s)
     expect(match.('/:e/d')).to eq(/^([^?\/#:]+#)?\/(?<e>[^?\/#:]+)\/d$/.to_s)
+    expect(match.('/:e/d//')).to eq(/^([^?\/#:]+#)?\/(?<e>[^?\/#:]+)\/d(?<*remain*>(\/.*)+)$/.to_s)
   end
 
   describe 'resolution' do
@@ -37,15 +58,25 @@ describe Kirb::RouteResolver do
     it 'fail resolve' do
       expect(resolve.('/a/b/c', '/x')).to eq([false, nil, nil])
       expect(resolve.('/:/d', '/x/y')).to eq([false, nil, nil])
+      expect(resolve.('/a/b//', '/a/b')).to eq([false, nil, nil])
     end
 
     it 'resolve' do
+      expect(resolve.('//', '/a/b')).to eq([true, {}, '/a/b'])
+      expect(resolve.('//', '/a/b/')).to eq([true, {}, '/a/b/'])
       expect(resolve.('/a/b/c', '/a/b/c')).to eq([true, {}, nil])
-      expect(resolve.('/a/b/c/', '/a/b/c/d')).to eq([true, {}, '/d'])
+      expect(resolve.('/a/b/c', '/a/b/c')).to eq([true, {}, nil])
+      expect(resolve.('/a/b/c//', '/a/b/c/d')).to eq([true, {}, '/d'])
+      expect(resolve.('/a/b//', '/a/b/c/d/e')).to eq([true, {}, '/c/d/e'])
+      expect(resolve.('/a/b/c//', '/a/b/c/d')).to eq([true, {}, '/d'])
       expect(resolve.('/:/d', '/x/d')).to eq([true, {}, nil])
       expect(resolve.('/:e/d', '/x/d')).to eq([true, {"e" => "x"}, nil])
-      expect(resolve.('/a/:something/:cool/', '/a/test/hey/another/path'))
+      expect(resolve.('/a/:something/:cool//', '/a/test/hey/another/path'))
         .to eq([true, {"something" => "test", "cool" => "hey"}, '/another/path'])
+    end
+
+    it 'fail resolve with method' do
+      expect(resolve.('POST#/:var/d', 'POST#GET#/avar/d')).to eq([false, nil, nil])
     end
 
     it 'resolve with method' do
